2acff0584276013caa50147f4204908c8c78152e
Robert <123roabel@gmail.com>
2024-04-26 21:31:43 -0400

    task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-""" yied a random number """
+""" mesure runtime """
 import asyncio
-import random
-from typing import Generator
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """yied a random number"""
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+async def measure_runtime() -> float:
+    """mesure runtime"""
+    start_time = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
+    return end_time - start_time
